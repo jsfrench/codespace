8e9194f7003538789fa7d0e2deac6a80a3cc3144
John French <jsfrench@users.noreply.github.com>
2022-11-21 17:17:23 +0000

    Mon, Nov 21, 2022, 5:17 PM +00:00

--- Exercise1.java
+++ Exercise1.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Exercise1 {
     public static void main(String[] args) {
         System.out.println("Hello");
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        System.out.println(num1);
     }
 }
